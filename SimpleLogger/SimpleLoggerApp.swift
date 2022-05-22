3dbbb53a3629c9fe9851cdd22a51776dd77e3534
David Rynn <davidrynn@gmail.com>
2022-05-22 00:31:46 -0400

    Initial Commit

---
+++ SimpleLogger/SimpleLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLoggerApp.swift
+//  SimpleLogger
+//
+//  Created by David Rynn on 5/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
